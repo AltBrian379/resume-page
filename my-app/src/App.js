0b42934b05f80cf09a1863f7ccdba02eca428e3f
AltBrian379 <brianaltamirano.business@gmail.com>
2023-12-28 19:45:53 -0800

    Added Card and Flexbox

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="">
+    <div className="d-flex flex-column align-items-center">
+      <Card/>
+      <Card/>
+      <Card/>
+      <Card/>
+
+    </div>
+    </div>
+  );
+}
+
+const Card = () => {
+  return (
+    <div className="card mb-3" style={{width: "18rem"}}>
+      <img src="..." className="card-img-top" alt="..."/>
+      <div className="card-body">
+        <h5 className="card-title">Card title</h5>
+        <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        <a href="#" className="btn btn-primary">Go somewhere</a>
+      </div>
     </div>
   );
 }
